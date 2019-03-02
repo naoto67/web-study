410e2620a416b956590f10307327afada1cc8281
naoto67 <uver.com67@gmail.com>
2019-03-03 01:57:33 +0900

    add annotate gem. annotate into each model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,11 @@
-class Tweet < ActiveRecord
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id       :bigint(8)        not null, primary key
+#  content  :string(255)      default(""), not null
+#  users_id :bigint(8)        not null
+#
+
+class Tweet < ApplicationRecord
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id      :bigint(8)        not null, primary key
+#  name    :string(255)      default(""), not null
+#  id_name :string(255)      default(""), not null
+#
+
 class User < ApplicationRecord
 end
